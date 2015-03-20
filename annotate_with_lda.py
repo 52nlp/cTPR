2aafebe6a1538c5d5bfcf35e58ce5e21e969e087
mago1chi <pseudony080@gmail.com>
2015-03-21 02:20:27 +0900

    added new function to LDA codes to run experiment

--- annotate_with_lda.py
+++ annotate_with_lda.py
@@ -2,32 +2,50 @@ from gensim import corpora
 from gensim import models
 import psycopg2
 
+DOC_NUM = 4
+
 DBPATH = "dbname=image_tagging host=localhost user=postgres"
 
 con = psycopg2.connect(DBPATH)
 concur = con.cursor()
 
-concur.execute("select tweet_id, word from preprocess order by tweet_id")
+#concur.execute("select tweet_id, word from preprocess order by tweet_id")
+
+concur.execute("delete from exp_rawlda")
+
+concur.execute("""select distinct a.tweet_id, a.word, a.count 
+  from preprocess as a, answer as b where a.tweet_id = b.tweet_id 
+  order by tweet_id""")
 
 res = concur.fetchall()
 
+tweet_id_list = []
 word_list = []
 tmp_tweet_id = 0
 tmp_word_list = []
 count = 0
 for each_res in res:
+  term_count = each_res[2]
+  
   if each_res[0] != tmp_tweet_id:
-    word_list.append(tmp_word_list)
+    if count > 0:
+      word_list.append(tmp_word_list)
     
     tmp_word_list = []
-    tmp_word_list.append(each_res[1])
+    for i in range(term_count):
+      tmp_word_list.append(each_res[1])
     
     tmp_tweet_id = each_res[0]
+    tweet_id_list.append(tmp_tweet_id)
     
     if count == len(res)-1:
       word_list.append(tmp_word_list)
   else:
-    tmp_word_list.append(each_res[1])
+    for i in range(term_count):
+      tmp_word_list.append(each_res[1])
+    
+    if count == len(res)-1:
+      word_list.append(tmp_word_list)
   
   count += 1
 
@@ -38,8 +56,57 @@ mm = corpora.MmCorpus('lda_corpus.mm')
 
 lda = models.LdaModel.load('lda_results.model')
 
-print(word_list[25])
-  
-print(dictionary.doc2bow(word_list[25]))
+#print(word_list[DOC_NUM], tweet_id_list[DOC_NUM])
+##print(dictionary.doc2bow(word_list[DOC_NUM]))
+#
+#dist = lda[dictionary.doc2bow(word_list[DOC_NUM])]
+#
+#top_topic_id = sorted(dist, key=lambda x: x[1], reverse=True)[0][0]
+#
+#for each_tuple in sorted(dist, key=lambda x: x[1], reverse=True):
+#  print(each_tuple)
+#
+#print(top_topic_id)
+#print(lda.show_topic(top_topic_id, 20))
+#
+#annotate_word_list = []
+#for each_topic_word in lda.show_topic(top_topic_id, 20):
+#  annotate_word_list.append(each_topic_word[1])
+#
+#annotate_word_set = set(annotate_word_list)
+#
+#final_annotate_word_set = annotate_word_set.intersection(set(word_list[DOC_NUM]))
+#
+#print(final_annotate_word_set)
 
-print(lda[dictionary.doc2bow(word_list[25])])
+for i in range(len(word_list)):
+  tweet_id = tweet_id_list[i]
+
+  dist = lda[dictionary.doc2bow(word_list[i])]
+
+  try:
+    top_topic_id = sorted(dist, key=lambda x: x[1], reverse=True)[0][0]
+  except:
+    print(word_list[i])
+    print(dist)
+    continue
+
+  annotate_word_list = []
+  for each_topic_word in lda.show_topic(top_topic_id, 20):
+    annotate_word_list.append(each_topic_word[1])
+
+  annotate_word_set = set(annotate_word_list)
+
+  final_annotate_word_set = annotate_word_set.intersection(set(word_list[i]))
+
+  for each_word in final_annotate_word_set:
+    concur.execute("""insert into exp_rawlda (tweet_id, tag) values (%s, %s)
+      """, [tweet_id, each_word])
+ 
+con.commit()
+
+concur.close()
+con.close()
+
+
+
--- make_corpus_for_lda.py
+++ make_corpus_for_lda.py
@@ -1,13 +1,26 @@
 from gensim import corpora
 from gensim import models
 import psycopg2
+import sys
 
 DBPATH = "dbname=image_tagging host=localhost user=postgres"
+
+topic_num_list = [30, 100, 200, 500]
+
+if len(sys.argv) != 2:
+  print("usage: python3.x make_corpus_for_lda.py <the number of topic>")
+  exit(1)
+
+topic_num = int(sys.argv[1])
+
+if not topic_num in topic_num_list:
+  print("the number of topic is whether 30, 100, 200, 500")
+  exit(1)
 
 con = psycopg2.connect(DBPATH)
 concur = con.cursor()
 
-concur.execute("select tweet_id, word from preprocess order by tweet_id")
+concur.execute("select tweet_id, word, count from preprocess order by tweet_id")
 
 res = concur.fetchall()
 
@@ -16,18 +29,27 @@ tmp_tweet_id = 0
 tmp_word_list = []
 count = 0
 for each_res in res:
+  term_count = each_res[2]
+  
   if each_res[0] != tmp_tweet_id:
-    word_list.append(tmp_word_list)
+    if count > 0:
+      word_list.append(tmp_word_list)
     
     tmp_word_list = []
-    tmp_word_list.append(each_res[1])
+    
+    for i in range(term_count):
+      tmp_word_list.append(each_res[1])
     
     tmp_tweet_id = each_res[0]
     
     if count == len(res)-1:
       word_list.append(tmp_word_list)
   else:
-    tmp_word_list.append(each_res[1])
+    for i in range(term_count):
+      tmp_word_list.append(each_res[1])
+    
+    if count == len(res)-1:
+      word_list.append(tmp_word_list)
   
   count += 1
 
@@ -39,6 +61,6 @@ id_corpus = [dictionary.doc2bow(x) for x in word_list]
 corpora.MmCorpus.serialize('lda_corpus.mm', id_corpus)
 mm = corpora.MmCorpus('lda_corpus.mm')
 
-lda = models.LdaModel(corpus=mm, id2word=dictionary, num_topics=30)
+lda = models.LdaModel(corpus=mm, id2word=dictionary, num_topics=topic_num)
 
 lda.save('lda_results.model')
